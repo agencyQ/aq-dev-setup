cd1c971f9fa8715177fa0f8342c4400e27c20cf4
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-12 11:43:58 -0400

    Seperate out removal of default apps and the boxstarter install

--- Scripts/baseSetup.ps1
+++ Scripts/baseSetup.ps1
@@ -1,7 +1,9 @@
-choco install -y boxstarter
-
 choco install -y git
+refreshenv
+
 choco install -y vscode
+refreshenv
+
 choco install -y googlechrome
 choco install -y firefox
 choco install -y notepadplusplus
@@ -10,87 +12,25 @@ choco install -y adobereader
 choco install -y filezilla
 choco install -y beyondcompare
 choco install -y winmerge
+choco install -y microsoftazurestorageexplorer
+choco install -y github-desktop
 
 
-#--- Uninstall unnecessary applications that come with Windows out of the box ---
-Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
 
-#Referenced to build script
-# https://docs.microsoft.com/en-us/windows/application-management/remove-provisioned-apps-during-update
-# https://github.com/jayharris/dotfiles-windows/blob/master/windows.ps1#L157
-# https://gist.github.com/jessfraz/7c319b046daa101a4aaef937a20ff41f
-# https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
-# https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
+try {
 
-function removeApp {
-  Param ([string]$appName)
-  Write-Output "Trying to remove $appName"
-  Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
-  Get-AppXProvisionedPackage -Online | Where-Object DisplayName -like $appName | Remove-AppxProvisionedPackage -Online
+
+  Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+  # will expand explorer to the actual folder you're in
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+  #adds things back in your left pane like recycle bin
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+  #opens PC to This PC, not quick access
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+  #taskbar where window is open for multi-monitor
+  Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
 }
-
-$applicationList = @(
-  "Microsoft.BingFinance"
-  "Microsoft.3DBuilder"
-  "Microsoft.BingNews"
-  "Microsoft.BingSports"
-  "Microsoft.BingWeather"
-  "Microsoft.CommsPhone"
-  "Microsoft.Getstarted"
-  "Microsoft.WindowsMaps"
-  "*MarchofEmpires*"
-  "Microsoft.GetHelp"
-  "Microsoft.Messaging"
-  "*Minecraft*"
-  "Microsoft.MicrosoftOfficeHub"
-  "Microsoft.OneConnect"
-  "Microsoft.WindowsPhone"
-  "Microsoft.WindowsSoundRecorder"
-  "*Solitaire*"
-  "Microsoft.MicrosoftStickyNotes"
-  "Microsoft.Office.Sway"
-  "Microsoft.XboxApp"
-  "Microsoft.XboxIdentityProvider"
-  "Microsoft.XboxGameOverlay"
-  "Microsoft.XboxGamingOverlay"
-  "Microsoft.ZuneMusic"
-  "Microsoft.ZuneVideo"
-  "Microsoft.NetworkSpeedTest"
-  "Microsoft.FreshPaint"
-  "Microsoft.Print3D"
-  "Microsoft.People*"
-  "Microsoft.Microsoft3DViewer"
-  "Microsoft.MixedReality.Portal*"
-  "*Skype*"
-  "*Autodesk*"
-  "*BubbleWitch*"
-  "king.com*"
-  "G5*"
-  "*Dell*"
-  "*Facebook*"
-  "*Keeper*"
-  "*Netflix*"
-  "*Twitter*"
-  "*Plex*"
-  "*.Duolingo-LearnLanguagesforFree"
-  "*.EclipseManager"
-  "ActiproSoftwareLLC.562882FEEB491" # Code Writer
-  "*.AdobePhotoshopExpress"
-);
-
-foreach ($app in $applicationList) {
-  removeApp $app
+catch {
+  Write-Host "Error setting Windows Explorer Options" -ForegroundColor "Red"
 }
-
-
-
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
-
-# will expand explorer to the actual folder you're in
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-#adds things back in your left pane like recycle bin
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-#opens PC to This PC, not quick access
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
-#taskbar where window is open for multi-monitor
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
