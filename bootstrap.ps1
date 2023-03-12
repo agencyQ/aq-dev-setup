c57dab684b7403676c870922a862cf16251c0885
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-12 18:57:58 -0400

    fix bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 . { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression ; Get-Boxstarter -Force
 
-Install-BoxstarterPackage -PackageName http://boxstarter.org/package/https://raw.githubusercontent.com/agencyQ/aq-dev-setup/main/Scripts/_index.ps1 -DisableReboots
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/agencyQ/aq-dev-setup/main/Scripts/_index.ps1
