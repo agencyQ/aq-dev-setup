5eae9d1f777ba9408d23bc2a2aafc655d46cc2db
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-12 17:24:30 -0400

    Update to polish out SQL Server Install

--- Scripts/sqlServer.ps1
+++ Scripts/sqlServer.ps1
@@ -1,4 +1,7 @@
 choco install -y sql-server-2022
+choco install -y sql-server-management-studio
+
+if (Test-PendingReboot) { Invoke-Reboot }
 
 Invoke-Sqlcmd -Query "EXEC sp_configure 'contained', 1;" -ServerInstance localhost
 Invoke-Sqlcmd -Query "RECONFIGURE;" -ServerInstance localhost
@@ -6,3 +9,5 @@ Invoke-Sqlcmd -Query "EXEC xp_instance_regwrite N'HKEY_LOCAL_MACHINE', N'Softwar
 Invoke-Sqlcmd -Query "ALTER LOGIN [sa] WITH DEFAULT_DATABASE=[master], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF" -ServerInstance localhost
 Invoke-Sqlcmd -Query "ALTER LOGIN [sa] WITH PASSWORD=N'Password123!'" -ServerInstance localhost
 Invoke-Sqlcmd -Query "ALTER LOGIN [sa] ENABLE" -ServerInstance localhost
+
+Restart-Service -Force MSSQLSERVER
