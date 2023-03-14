d69718f5bc1ba401d735e17a55c8b940c0ba0bb0
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-14 18:36:13 -0400

    Update Visual studio to include .NET framework pieces for Sitecore installation as well.

--- Scripts/visualStudio.ps1
+++ Scripts/visualStudio.ps1
@@ -1,2 +1,6 @@
 choco install -y visualstudio2022professional
 choco install -y DotPeek
+
+cinst dotnet-6.0-sdk -y
+cinst dotnet-6.0-windowshosting-sdk -y
+cinst vcredist2015 -y
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 . { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression ; Get-Boxstarter -Force
 
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/agencyQ/aq-dev-setup/main/Scripts/_index.ps1
