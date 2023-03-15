21602fd6bb1d32e0fc1eb1c89db47582eec524e6
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-14 21:59:59 -0400

    Update SystemSettings.ps1

--- Scripts/SystemSettings.ps1
+++ Scripts/SystemSettings.ps1
@@ -33,6 +33,7 @@ try {
   Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
   #taskbar where window is open for multi-monitor
   Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+  reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 }
 catch {
   Write-Host "Error setting Windows Explorer Options" -ForegroundColor "Red"
