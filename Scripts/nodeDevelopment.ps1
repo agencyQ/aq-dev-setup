d30fd2558f094cae57cf626ea9ff39251e895f0f
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-16 08:48:56 -0400

    Stop mucking with the NVS stuff since we are using dedicated environments

--- Scripts/nodeDevelopment.ps1
+++ Scripts/nodeDevelopment.ps1
@@ -1,16 +1,1 @@
-choco install -y nvs
-$env:NVS_HOME = "$env:LocalAppData\nvs"
-
-Push-Location $env:NVS_HOME
-
-try {
-  . "$env:NVS_HOME\nvs.ps1" install
-  . "$env:NVS_HOME\nvs.ps1" add lts
-  . "$env:NVS_HOME\nvs.ps1" use lts
-}
-catch {
-  Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
-}
-finally {
-  Pop-Location
-}
+choco install nodejs-lts - y
